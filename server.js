e808f37c3f5ab5477439aa315ccb3c16c56298cf
Benoît Hubert <benoithubert@gmail.com>
2016-10-27 12:30:18 +0200

    Promise.all to be replaced with .map

--- server.js
+++ server.js
@@ -8,13 +8,21 @@ var promises  = [];
 var readFileAsync = Promise.promisify(fs.readFile);
 
 function loadTemplate(template) {
-  return readFileAsync(__dirname + '/templates/' + template);
+  // return function() {
+    return readFileAsync(__dirname + '/templates/' + template + '.html')
+    .then(function(result) { return result.toString(); });
+  // };
 }
 
 templates.forEach(function(template) {
   // console.log(template);
   promises.push(loadTemplate(template));
-})
+});
+console.log(promises);
+Promise.all(promises)
+.then(function(result) {
+  console.log(result);
+});
 
 app.get('/admin', function(req, res) {
   // res.json({ pouet: 'pouet' });
